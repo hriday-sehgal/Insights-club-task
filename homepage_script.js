7974379b98aa237530a2a9b8a067e6881a948d19
Hriday Sehgal <102664313+hriday-sehgal@users.noreply.github.com>
2023-05-21 14:44:37 +0530

    Update homepage_script.js

--- homepage_script.js
+++ homepage_script.js
@@ -3,8 +3,8 @@ function fetchUserData() {
  
     const userData = {
       profilePicture: 'https://scontent-bom1-1.xx.fbcdn.net/v/t39.30808-1/297802466_125862313502620_5323529870175982280_n.jpg?stp=dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=c6021c&_nc_ohc=Fhf9J7fbrmYAX_OyAM7&_nc_ht=scontent-bom1-1.xx&oh=00_AfBCy4nUsGymjgJMPIb_3SXoN1n59-LtUCLwZr1S04eOHA&oe=646E157F',
-      fullName: 'John Doe',
-      email: 'john.doe@example.com'
+      fullName: 'Hriday Sehgal',
+      email: 'hridaysehgal@gmail.com'
     };
   
     return userData;
